2809fd28f2e4cf5d4194604b854f44f60f2d1771
Carlos Soto <odoo.soporte@gmail.com>
2020-05-04 13:25:03 -0400

    Validated parameter id with where and regex

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts/{id}', function ($id) {
+    return "Calling the Post # $id";
+})->where('id', '[0-9]+');
